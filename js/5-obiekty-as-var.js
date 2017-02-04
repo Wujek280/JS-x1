30085e6342e980d8dce74bf7a71d01162e030bea
Wujek <mjwojcik94@gmail.com>
2017-02-04 11:08:45 +0100

    math, round

---
+++ js/5-obiekty-as-var.js
@@ -0,0 +1,12 @@
+   'use strict';
+
+//Obiekty
+   
+   var Student = {
+
+      name: "Marcin",
+      height: 184,
+      print: function() {console.log( console.log(this.name) )}
+   }
+   
+   Student.print();
---
+++ js/6-math.js
@@ -0,0 +1,5 @@
+'use strict';
+
+console.log(Math.PI);
+
+console.log( round(Math.cos(0.01), 2 )); // napraw
