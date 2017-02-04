74d2fe923a0b853639f8b8c657cc4fd5bad2e1f4
Wujek <mjwojcik94@gmail.com>
2017-02-04 11:25:07 +0100

    switch

--- js/7-conditional.js
+++ js/7-conditional.js
@@ -2,9 +2,11 @@
 
 var wzrostMateusza = 190;
 var wzrostKaji = 152;
+var kolor = 'czerwony';
+
 
 /*
-Waruunek IF
+Waruunek IF, IF ELSE
 */
 
 if (wzrostKaji < wzrostMateusza) console.log("Kaja niższa")
@@ -13,3 +15,17 @@ else console.log("Mateusz nizszy")
 if ( wzrostKaji > wzrostMateusza ) console.log("kaja jest wyzsza")
  else if ( wzrostKaji == wzrostMateusza ) console.log("są rowni")
     else console.log("mateusz jest wyższy")
+    
+    
+/* 
+Słicz
+*/
+    
+switch(kolor) {
+   case 'czerwony': console.log("Kolor jest czerwony"); break;
+      
+   case 'zielony': console.log("Kolor jest zielony")
+   break;
+      
+   default: console.log("Inny kolor...");
+}
