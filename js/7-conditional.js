4344c7b991354a1d786a68ffbea195034760a3d7
Wujek <mjwojcik94@gmail.com>
2017-02-04 11:18:01 +0100

    elsif

---
+++ js/7-conditional.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var wzrostMateusza = 190;
+var wzrostKaji = 152;
+
+/*
+Waruunek IF
+*/
+
+if (wzrostKaji < wzrostMateusza) console.log("Kaja niższa")
+else console.log("Mateusz nizszy")
+
+if ( wzrostKaji > wzrostMateusza ) console.log("kaja jest wyzsza")
+ else if ( wzrostKaji == wzrostMateusza ) console.log("są rowni")
+    else console.log("mateusz jest wyższy")
