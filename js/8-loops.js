6c6b3e25d81939c6e5f4aeae6bcb34fdde54e30d
Wujek <mjwojcik94@gmail.com>
2017-02-04 11:49:20 +0100

    post i preinkrementacja

--- js/8-loops.js
+++ js/8-loops.js
@@ -1,1 +1,40 @@
 'use strict';
+/*
+
+Petla FOR
+
+*/
+
+//for (var i=-4; i<=10; i++) console.log(i+" ");
+
+
+/*
+
+Petla WHILE
+
+*/
+
+//var it = 0;
+//
+//while( it<9 ) {
+//   console.log(it); 
+//   it++;
+//}
+
+/*
+
+Petla DO WHILE
+
+*/
+
+var iter = 11;
+
+do { console.log(iter) }
+while (iter < 10)
+
+//Post vs preinkrememntacja
+ console.log("PREINKREMENTACJA");
+ for (var i=-4; i<=10; ) console.log(++i);
+
+ console.log("POSTINKREMENTACJA");
+ for (var i=-4; i<=10; ) console.log(i++);
