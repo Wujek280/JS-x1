3b3ed44cf0ef67a4558d6a6eececcacfbcc49b1f
Wujek <mjwojcik94@gmail.com>
2017-02-04 13:41:06 +0100

    fankszyn abc returny

---
+++ js/10-funkcje.js
@@ -0,0 +1,34 @@
+'use strict';
+
+//var liczba1 = 10;
+//var liczba2 = 25.11;
+//var wynik;
+//
+//
+//
+//function dodajLiczby(a, b) {
+//   return a+b;
+//}
+//
+//wynik = dodajLiczby(liczba1, liczba2);
+//
+//console.log(wynik)
+
+var a=2;
+var b=-3;
+var c=4.01;
+
+function multiply3(a, b, c) {
+   return a*b*c;   
+}
+
+function writeMe(param){
+   
+   if (ans >= 0) console.log(param)
+   else document.write(param)
+   
+}
+
+var ans;
+ans = multiply3(a,b,c)
+writeMe(ans)
