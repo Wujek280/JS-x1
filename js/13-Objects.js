e14a0750fe0a224c50c59a387066a0b2163960a1
Wujek <mjwojcik94@gmail.com>
2017-02-04 16:53:16 +0100

    instancje

--- js/13-Objects.js
+++ js/13-Objects.js
@@ -1,1 +1,19 @@
 'use strict';
+
+var kaja = {
+   imie: "Kaja",
+   wzrost: 152,
+   przedstawOsobe: function() {console.log("Jestem "+ this.imie)}
+}
+
+var krystian = {
+   imie: "Krystian",
+   wzrost: 168,
+   przedstawOsobe: function() {console.log("Jestem "+ this.imie)}
+}
+
+
+console.log(kaja.wzrost+" cm")
+
+kaja.przedstawOsobe();
+krystian.przedstawOsobe();
