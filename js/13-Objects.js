ce91924ab07f2da69187540d0b71462832e1b1e1
Wujek <mjwojcik94@gmail.com>
2017-02-05 09:01:13 +0100

    lastedit

--- js/13-Objects.js
+++ js/13-Objects.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var kaja = {
+/*var kaja = {
    imie: "Kaja",
    wzrost: 152,
    przedstawOsobe: function() {console.log("Jestem "+ this.imie)}
@@ -16,4 +16,30 @@ var krystian = {
 console.log(kaja.wzrost+" cm")
 
 kaja.przedstawOsobe();
-krystian.przedstawOsobe();
+krystian.przedstawOsobe();*/
+
+/*-------CLASS--------------*/
+
+function Osoba(imie, nazwisko) {
+   this.imie = imie;
+   this.nazwisko = nazwisko;
+   this.wzrost = null;
+   this.oczy;
+   this.wyswietlInfo = function() {
+      console.log(   "/*---------------*/ \n"+
+                     "Imię: " +this.imie+ "\n"+
+                     "Nazwisko: " +this.nazwisko+ "\n"+
+                     "Wzrost: " +this.wzrost+ "\n"+
+                     "Oczy: " +this.oczy+
+                     "\n/*---------------*/ ")
+   }
+}
+
+/*-------------------------*/
+
+var krystian = new Osoba('Krystian', 'Dziopa');
+krystian.wyswietlInfo();
+
+var prezydent = new Osoba('Andrzej', 'Duda');
+prezydent.wyswietlInfo();
+
