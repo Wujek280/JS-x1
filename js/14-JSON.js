e60cd0d6394fe70e3975f08d87e0025ee7f32c56
Wujek <mjwojcik94@gmail.com>
2017-03-23 23:34:53 +0100

    minor

--- js/14-JSON.js
+++ js/14-JSON.js
@@ -5,10 +5,10 @@
   var jsonCars = {
    "samochody":
       [
-         {marka: "Peugeot 206", moc: 120, pojemnosc: 1.2},
-         {marka: "Tesla Roadster", moc: 300, pojemnosc: 0},
-         {marka: "BMW M3", moc: 360, pojemnosc: 4},
-         {marka: "Trabant", moc: 89, pojemnosc: 1.7}
+         {marka: "Peugeot 206", moc: 120, pojemnosc: 1.2 },
+         {marka: "Tesla Roadster", moc: 300, pojemnosc: 0 },
+         {marka: "BMW M3", moc: 360, pojemnosc: 4 },
+         {marka: "Trabant", moc: 89, pojemnosc: 1.7 }
       ]
       
   }
